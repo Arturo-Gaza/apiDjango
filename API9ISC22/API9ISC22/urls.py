7cc29401bf7aab0615cbecb4109a8c2f5697b200
Arturo-Gaza <2019452038@teschi.edu.mx>
2023-09-09 21:50:16 -0600

    Mensaje descriptivo del commit

--- API9ISC22/API9ISC22/urls.py
+++ API9ISC22/API9ISC22/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+#from django.contrib import admin
 from django.urls import path
 from api.views import Home 
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('',Home.as_view,name='index'),
+    path('', Home.as_view(),name='index'),
 ]
